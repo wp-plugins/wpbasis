61faf8803ef29f2951b766a26f28c5c4ce429036
wpmarkuk <wpmarkuk@b8457f37-d9ea-0310-8a92-e5e31aec5664>
2015-09-17 09:12:17 +0000

    Hotfix for correcting typo in the settings screen.
    git-svn-id: https://plugins.svn.wordpress.org/wpbasis/tags/1.6.4@1247449 b8457f37-d9ea-0310-8a92-e5e31aec5664

---
+++ deprecated/site-options.php
@@ -0,0 +1,288 @@
+<?php
+/**
+ * function wpbasis_register_site_options
+ * register the option settings for the site options page
+ */
+function wpbasis_register_site_options() {
+	
+	/* create an array of the default settings, making it filterable */
+	$wpbasis_registered_site_option_settings = apply_filters(
+		'wpbasis_register_site_option_settings',
+		array(
+			'wpbasis_twitter_url' => 'wpbasis_twitter_url',
+			'wpbasis_facebook_url' => 'wpbasis_facebook_url',
+			'wpbasis_linkedin_url' =>'wpbasis_linkedin_url',
+			'wpbasis_contact_email' => 'wpbasis_contact_email',
+			'wpbasis_tel_no' => 'wpbasis_tel_no',
+			'wpbasis_footer_text' => 'wpbasis_footer_text'
+		)
+	);
+
+	/* loop through each setting to register */
+	foreach( $wpbasis_registered_site_option_settings as $key => $value ) {
+
+		/* register the setting */
+		register_setting( 'wpbasis_site_options', $value );
+
+	}
+	
+}
+
+/* hook function into admin_init */
+add_action( 'admin_init', 'wpbasis_register_site_options' );
+
+/**
+ * Function wpbasis_add_site_options()
+ * Adds a new menu item for the plugin settings.
+ */
+function wpbasis_add_site_options() {
+	
+	/* if the current user is not a wpbasis super user */
+	if( ! wpbasis_is_wpbasis_user() ) {
+
+		/* add a new menu item linking to our new dashboard page */
+		add_menu_page(
+			'Site Options',
+			'Site Options',
+			'edit_pages',
+			'wpbasis_site_options',
+			'wpbasis_site_options_content',
+			'dashicons-admin-generic',
+			99
+		);
+
+	/* current user is a pixel team member */
+	} else {
+
+		/* a site options as sub page of settings */
+		add_submenu_page(
+			'options-general.php',
+			'Site Options',
+			'Site Options',
+			'edit_pages',
+			'wpbasis_site_options',
+			'wpbasis_site_options_content'
+		);
+
+	}
+
+}
+
+add_action( 'admin_menu', 'wpbasis_add_site_options' );
+
+
+/**
+ * Function wpbasis_site_options_content()
+ * Creates the output markup for the added site options page
+ */
+function wpbasis_site_options_content() {
+
+	?>
+	
+	<div class="wrap">
+		
+		<h2>Site Options</h2>
+		
+		<?php
+
+			/**
+			 * @hooked wpbasis_before_site_options_form
+			 */
+			do_action( 'wpbasis_before_site_options_form' );
+
+		?>
+		
+		<form method="post" action="options.php">
+		
+			<?php settings_fields( 'wpbasis_site_options' ); ?>
+			
+			<table class="form-table">
+			
+				<tbody>
+					
+					<?php
+
+						/* create empty filterable array for plugins to add own settings */
+						$wpbasis_site_option_settings = apply_filters(
+							'wpbasis_site_option_settings',
+							array(
+								'wpbasis_twitter_url' => array(
+									'setting_name' => 'wpbasis_twitter_url',
+									'setting_label' => 'Twitter URL',
+									'setting_description' => 'Enter the URL for your Twitter page.',
+									'setting_type' => 'text',
+									'setting_class' => 'twitter',
+								),
+								'wpbasis_facebook_url' => array(
+									'setting_name' => 'wpbasis_facebook_url',
+									'setting_label' => 'Facebook URL',
+									'setting_description' => 'Enter the URL for your Facebook page.',
+									'setting_type' => 'text',
+									'setting_class' => 'facebook',
+								),
+								'wpbasis_linkedin_url' => array(
+									'setting_name' => 'wpbasis_linkedin_url',
+									'setting_label' => 'LinkedIn URL',
+									'setting_description' => 'Enter the URL for your LinkedIn page.',
+									'setting_type' => 'text',
+									'setting_class' => 'linkedin',
+								),
+								'wpbasis_contact_email' => array(
+									'setting_name' => 'wpbasis_contact_email',
+									'setting_label' => 'Contact Email',
+									'setting_description' => 'Enter a contact email here, this may be used on the site for people to get in touch with you.',
+									'setting_type' => 'text',
+									'setting_class' => 'email',
+								),
+								'wpbasis_tel_no' => array(
+									'setting_name' => 'wpbasis_tel_no',
+									'setting_label' => 'Telephone Number',
+									'setting_description' => 'Please enter your contact telephone number here, which may be displayed on the site depending on your design.',
+									'setting_type' => 'text',
+									'setting_class' => 'telno',
+								),
+								'wpbasis_footer_text' => array(
+									'setting_name' => 'wpbasis_footer_text',
+									'setting_label' => 'Footer Text',
+									'setting_description' => 'Enter text here to display in the footer of your site. You could include a Copyright notice for example.',
+									'setting_type' => 'wysiwyg',
+									'setting_class' => 'footer_text',
+									'media_buttons' => apply_filters( 'wpbasis_footer_text_media_buttons', false ),
+									'textarea_rows' => 5,
+								),
+							)
+						);
+
+						/* check we have settings items to output */
+						if( ! empty( $wpbasis_site_option_settings ) ) {
+
+							/* loop through each feature control output */
+							foreach( $wpbasis_site_option_settings as $wpbasis_site_option_setting ) {
+
+								?>
+						
+								<tr valign="top" class="wpbasis-setting wpbasis-setting-<?php echo esc_attr( $wpbasis_site_option_setting[ 'setting_class' ] ); ?>">
+									<th scope="row">
+										<label for="<?php echo esc_attr( $wpbasis_site_option_setting[ 'setting_name' ] ); ?>"><?php echo $wpbasis_site_option_setting[ 'setting_label' ]; ?></label>
+									</th>
+									<td>
+										
+										<?php
+
+											/* setup a swith statement to output based on setting type */
+											switch( $wpbasis_site_option_setting[ 'setting_type' ] ) {
+
+												/* if the type is set to select input */
+											    case 'select':
+
+											    	?>
+											    	<select name="<?php echo $wpbasis_site_option_setting[ 'setting_name' ]; ?>" id="<?php echo $wpbasis_site_option_setting[ 'setting_name' ]; ?>">
+											    	
+											    	<?php
+
+											    	/* get the setting options */
+											    	$wpbasis_setting_options = $wpbasis_site_option_setting[ 'setting_options' ];
+
+											        /* loop through each option */
+											        foreach( $wpbasis_setting_options as $wpbasis_setting_option ) {
+
+												        ?>
+												        <option value="<?php echo esc_attr( $wpbasis_setting_option[ 'value' ] ); ?>" <?php selected( get_option( $wpbasis_site_option_setting[ 'setting_name' ] ), $wpbasis_setting_option[ 'value' ] ); ?>><?php echo $wpbasis_setting_option[ 'name' ]; ?></option>
+														<?php
+
+											        }
+
+											        ?>
+											    	</select>
+											        <?php
+
+											        /* break out of the switch statement */
+											        break;
+
+											    /* if the type is set to a textarea input */  
+											    case 'textarea':
+
+											    	?>
+											        <textarea name="<?php echo $wpbasis_site_option_setting[ 'setting_name' ]; ?>" rows="4" cols="50" id="<?php echo $wpbasis_site_option_setting[ 'setting_name' ]; ?>" class="regular-text"><?php echo get_option( $wpbasis_site_option_setting[ 'setting_name' ] ) ?></textarea>
+											        <?php
+
+											        /* break out of the switch statement */
+											        break;
+
+											    case 'wysiwyg':
+
+											    	/* set some settings args for the editor */
+											    	$wpbasis_editor_settings = array(
+											    		'textarea_rows' => $wpbasis_site_option_setting[ 'textarea_rows' ],
+											    		'media_buttons' => $wpbasis_site_option_setting[ 'media_buttons' ],
+											    	);
+
+											    	/* get current content for the wysiwyg */
+											    	$wpbasis_wysiwyg_content = get_option( $wpbasis_site_option_setting[ 'setting_name' ] );
+
+											    	/* display the wysiwyg editor */
+											    	wp_editor( $wpbasis_wysiwyg_content, $wpbasis_site_option_setting[ 'setting_name' ], $wpbasis_editor_settings );
+
+											    	break;
+
+											    /* any other type of input - treat as text input */ 
+											    default:
+													?>
+													<input type="text" name="<?php echo $wpbasis_site_option_setting[ 'setting_name' ]; ?>" id="<?php echo $wpbasis_site_option_setting[ 'setting_name' ]; ?>" class="regular-text" value="<?php echo get_option( $wpbasis_site_option_setting[ 'setting_name' ] ) ?>" />
+													<?php
+
+											}
+
+										?>
+										
+										<p class="description"><?php echo $wpbasis_site_option_setting[ 'setting_description' ]; ?></p>
+									</td>
+								</tr>
+								
+								<?php
+
+							}
+
+						}
+
+					?>
+				</tbody>
+				
+			</table>
+			
+			<p class="submit">
+				<input type="submit" name="submit" id="submit" class="button-primary" value="Save Changes">
+			</p>
+			
+		</form>
+		
+		<?php
+
+		/**
+		 * @hooked wpbasis_after_site_options_form
+		 */
+		do_action( 'wpbasis_after_site_options_form' );
+
+		?>
+		
+	</div><!- // wrap -->
+	
+	<?php
+
+}
+
+/**
+ * Function wpbasis_site_options_intro()
+ * Output intro text on the site options page
+ * @hooked wpbasis_before_site_options_form
+ */
+function wpbasis_site_options_intro() {
+	
+	/* output filterable intro text */
+	$wpbasis_site_options_intro = '<p>Below you can set some basic options for your site. Some of these options are used to display content on the front end, for example your telephone number may appear depending on your design.</p>';
+	
+	echo apply_filters( 'wpbasis_site_option_intro', $wpbasis_site_options_intro );
+	
+}
+
+add_action( 'wpbasis_before_site_options_form', 'wpbasis_site_options_intro', 10 );
---
+++ wpbasis.php
@@ -0,0 +1,131 @@
+<?php
+/*
+Plugin Name: WP Basis
+Plugin URI: https://github.com/wpmark/wpbasis
+Description: WP Basis provides the basis of a WordPress site by giving you access to the types of functions you end up writing for all sites. It also gives modifications to the WordPress dashboard which make it easier to work with for your clients.
+Author: Mark Wilkinson
+Author URI: http://markwilkinson.me
+Version: 1.6.4
+
+This program is free software; you can redistribute it and/or modify
+it under the terms of the GNU General Public License as published by
+the Free Software Foundation; either version 2 of the License, or
+(at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+*/
+
+/* exist if directly accessed */
+if( ! defined( 'ABSPATH' ) ) exit;
+
+/* define variable for path to this plugin file. */
+define( 'WPBASIS_LOCATION', dirname( __FILE__ ) );
+define( 'WPBASIS_LOCATION_URL', plugins_url( '', __FILE__ ) );
+
+/**
+ * include the necessary functions file for the plugin
+ */
+require_once dirname( __FILE__ ) . '/functions/template-functions.php';
+require_once dirname( __FILE__ ) . '/functions/counters.php';
+require_once dirname( __FILE__ ) . '/deprecated/deprecated.php';
+
+/**
+ * include the admin functions 
+ */
+require_once dirname( __FILE__ ) . '/functions/admin/admin-profile.php';
+require_once dirname( __FILE__ ) . '/functions/admin/admin-menus.php';
+require_once dirname( __FILE__ ) . '/functions/admin/admin-menus-content.php';
+require_once dirname( __FILE__ ) . '/functions/admin/admin.php';
+require_once dirname( __FILE__ ) . '/functions/admin/admin-bar.php';
+require_once dirname( __FILE__ ) . '/functions/admin/admin-display.php';
+
+/**
+ * include the plugin defaults
+ */
+require_once dirname( __FILE__ ) . '/functions/defaults/default-tabs.php';
+require_once dirname( __FILE__ ) . '/functions/defaults/default-settings.php';
+require_once dirname( __FILE__ ) . '/functions/defaults/default-capabilities.php';
+
+/**
+ * deal with legacy code here
+ * load post type descriptions - filterable
+ * add the site options - filterable
+ */
+if( apply_filters( 'wpbasis_use_post_type_descriptions', false ) == true ) {
+	require_once dirname( __FILE__ ) . '/deprecated/post-type-descriptions.php';
+}
+
+if( apply_filters( 'wpbasis_show_site_options', false ) == true ) {
+	require_once dirname( __FILE__ ) . '/deprecated/site-options.php';
+}
+
+/**
+ * Function wpbasis_on_activation()
+ * On plugin activation makes current user a wpbasis user and
+ * sets an option to redirect the user to another page.
+ */
+function wpbasis_on_activation() {
+	
+	/* get the current users, user ID */
+	$wpbasis_user_id = get_current_user_id();
+	
+	/* make the user a wpbasis super user */
+	update_usermeta( $wpbasis_user_id, 'wpbasis_user', 1 );
+	
+	/* set option to initialise the redirect */
+	add_option( 'wpbasis_activation_redirect', true );
+	
+}
+
+register_activation_hook( __FILE__, 'wpbasis_on_activation' );
+
+/**
+ * Function wp_basis_activation_redirect()
+ * Redirects user to the settings page for wp basis on plugin
+ * activation.
+ */
+function wp_basis_activation_redirect() {
+	
+	/* check whether we should redirect the user or not based on the option set on activation */
+	if( true == get_option( 'wpbasis_activation_redirect' ) ) {
+		
+		/* delete the redirect option */
+		delete_option( 'wpbasis_activation_redirect' );
+		
+		/* redirect the user to the wp basis settings page */
+		wp_redirect( admin_url( 'admin.php?page=wpbasis_settings' ) );
+		exit;
+		
+	}
+	
+}
+
+add_action( 'admin_init', 'wp_basis_activation_redirect' );
+
+/**
+ * Function wpbasis_enqueue_scripts()
+ * Adds plugins scripts and styles
+ */
+function wpbasis_enqueue_scripts() {
+
+	/* load the global variable to see which admin page we are on */
+	global $pagenow;
+
+	/* check whether the current admin page is wpbasis dashboard page */
+	if( $pagenow == 'admin.php' ) {
+	
+		/* site js scripts */
+		wp_enqueue_script( 'wpbasis_js', plugins_url( 'assets/js/wpbasis-min.js', __FILE__ ), 'jquery' );
+		
+		/* register the stylesheet */
+		wp_enqueue_style( 'wpbasis_admin_css', plugins_url( 'assets/css/wpbasis.css', __FILE__ ) );
+
+	}
+
+}
+
+add_action( 'admin_enqueue_scripts', 'wpbasis_enqueue_scripts' );
